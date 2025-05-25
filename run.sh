6debe2f1b4967c18d1ee1e731871c9bb999651a8
Erik Pettersson <erikpettersson02@gmail.com>
2025-05-26 00:09:09 +0200

    update
    the setup script which worked for me and the run script to start running the app with gpu acceleration.

---
+++ run.sh
@@ -0,0 +1,4 @@
+!#/bin/bash
+set -e
+
+python3.10 run.py --execution-provider coreml
--- setup-Mac-ARM.sh
+++ setup-Mac-ARM.sh
@@ -1,3 +1,12 @@
 !#/bin/bash
 set -e
 
+brew install python@3.10
+brew install python-tk@3.10
+python3.10 -m venv venv
+source venv/bin/activate
+
+pip install -r requirements.txt
+
+pip uninstall onnxruntime onnxruntime-silicon
+pip install onnxruntime-silicon==1.13.1
